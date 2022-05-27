cb84f913d5b175dd19f49c288a9913605cb8bda9
VenoMidas <schuma1022@gmail.com>
2022-05-27 09:33:58 -0500

    Complete lessons 1 - 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  let celcius = (f-32)*(5/9);
+  let rounded = Math.round(celcius * 10) / 10;
+  return rounded;
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let fahrenheit = c*(9/5)+32;
+  let rounded = Math.round(fahrenheit * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
@@ -11,3 +15,7 @@ module.exports = {
   ftoc,
   ctof
 };
+
+
+/* degree F = C*(9/5)+32 */
+/* degree C = (F-32)*(5/9) */ 
