30cc731122469f723398d1e11c583dd97d35d611
VenoMidas <schuma1022@gmail.com>
2022-06-07 22:13:21 -0500

    Complete exercizes

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((firstItem, nextItem) => firstItem * nextItem);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = num;
+  if(num === 0 || num === 1)
+    return 1;
+  while (num > 1) {
+    num--;
+    result *= num;
+  }  
+  return result;
 };
 
 // Do not edit below this line
